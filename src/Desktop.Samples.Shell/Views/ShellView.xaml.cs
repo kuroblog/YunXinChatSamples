9922bbc4b9404aaf681604f503cea22c3aa7cb5f
zhaoyuqi <lancet@SHL081>
2022-01-13 18:01:14 +0800

    # add metroapps control

--- src/Desktop.Samples.Shell/Views/ShellView.xaml.cs
+++ src/Desktop.Samples.Shell/Views/ShellView.xaml.cs
@@ -1,12 +1,12 @@
 ﻿using Desktop.Samples.Common;
 using Desktop.Samples.Shell.ViewModels;
+using MahApps.Metro.Controls;
 using Microsoft.Practices.Prism.Logging;
 using System;
-using System.Windows;
 
 namespace Desktop.Samples.Shell.Views
 {
-    public partial class ShellView : Window
+    public partial class ShellView : MetroWindow //System.Windows.Window
     {
         private readonly ILoggerFacade _logger;
 
