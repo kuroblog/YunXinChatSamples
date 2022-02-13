983bcc18762b645b38e66eefc10d763943583763
Thinker.Q <kuro_q@live.com>
2022-02-14 00:05:01 +0800

    # regionnames

---
+++ src2/CustomControls.Samples.WPF/Utils/RegionNames.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomControls.Samples.WPF.Utils
+{
+    public class RegionNames
+    {
+        public static string Root => nameof(Root);
+
+        public static string Main => nameof(Main);
+
+        public static string Content => nameof(Content);
+    }
+}
